f1f326a9ad72c95742a60e8bbaf0b8a0907385ba
ajitkumar-yadav-20226 <ajitkumaryadav20226@gmail.com>
2025-08-14 23:49:46 +0530

    Add files via upload

--- Arraylist.java
+++ Arraylist.java
@@ -1,45 +1,26 @@
-//Arraylist and its operation
-import java.util.ArrayList;
+//find maximum
+//import java.util.collection;
+import java.util.*;
 public class Arraylist{
+    public static void swap(ArrayList<Integer>list,int idx1,int idx2){
+        int temp = list.get(idx1);
+        list.set(idx1,list.get(idx2));
+        list.set(idx2,temp);
+    }
     public static void main(String args[]){
         ArrayList<Integer>list = new ArrayList<>();
-        list.add(2);
         list.add(4);
+        list.add(5);
         list.add(6);
-        list.add(8);
-        list.add(10);
+        list.add(7);
+        int idx1 = 1,idx2 = 3;
         System.out.println(list);
-
-        //remove
-        list.remove(2);
+        swap(list,idx1,idx2);
         System.out.println(list);
-
-        //get
-        int element = list.get(2);
-        System.out.println(element);
-
-        //set
-        list.set(2,12);
-        System.out.println(list);
-
-        //contains
-        System.out.println(list.contains(1));
-        System.out.println(list.contains(10));
-
-        //size of Arraylist
-        
-        System.out.println(list.size());
-
-        //To print increase order
-        for(int i=0;i<list.size();i++){
-            System.out.print(list.get(i)+" ");
-        }
-        System.out.println();
-
-        //To print the decerasing order
-        for(int i=list.size()-1;i>=0;i--){
-            System.out.print(list.get(i)+" ");
-        }
-        System.out.println();
+        //int max = Integer.MIN_VALUE;
+        //for(int i=0;i<list.size();i++){
+          //  max = Math.max(max,list.get(i));
+            //System.out.println(max);
+        //}
     }
 }
